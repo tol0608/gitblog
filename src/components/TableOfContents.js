9e1cccfbaaa38790061c552096074f343c8bf879
jaehyeonlee <jerry0608s@gmail.com>
2023-06-27 23:38:36 +0900

    Feat & Design : TOC 마우스 스크롤 및 CSS 효과 수정,
    TOC 마우스 스크롤 효과는 아직 미구현이라 추후 연구가 더 필요해보임

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,27 +1,71 @@
-import React from "react"
-import * as styles from "../styles/toc.module.css"
+import React, { useEffect } from "react";
+import * as styles from "../styles/toc.module.css";
 
 const TableOfContents = ({ content }) => {
     const handleClick = (event) => {
         const target = event.target;
         // 현재 활성화된 TOC 항목을 비활성화
-        const activeItem = document.querySelector(`.${styles.activeItem_hashedString}`);
+        const activeItem = document.querySelector(`.${styles.activeItem}`);
         if (activeItem) {
-            activeItem.classList.remove(styles.activeItem_hashedString);
+            activeItem.classList.remove(styles.activeItem);
         }
         // 클릭한 TOC 항목을 활성화
-        target.classList.add(styles.activeItem_hashedString);
+        target.classList.add(styles.activeItem);
     };
 
     const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
             handleClick(event);
         }
     };
 
+    useEffect(() => {
+        const scrollHandler = () => {
+            const toc = document.getElementsByTagName("aside");
+            if (!toc || toc.length < 0 || !toc[0] || !toc[0].style || toc[0].offsetWidth === 0) {
+                return;
+            }
+
+            const anchor_holder = document.getElementsByClassName("anchor-header");
+            if (!anchor_holder || anchor_holder.length <= 0) {
+                return;
+            }
+            let selected_anchor = null;
+            const anchor_holder_arr = Array.from(anchor_holder);
+            for (let a of anchor_holder_arr) {
+                if (a.getBoundingClientRect().top > -30) {
+                    selected_anchor = a.getAttribute("href");
+                    break;
+                }
+            }
+            if (!selected_anchor) {
+                selected_anchor = anchor_holder_arr[anchor_holder_arr.length - 1].getAttribute("href");
+            }
+            document.querySelectorAll("aside a.selected").forEach((a) => {
+                a.classList.remove("selected");
+            });
+            if (selected_anchor) {
+                const toc_selected = document.querySelector("aside a[href='" + decodeURIComponent(selected_anchor) + "']");
+                toc_selected && toc_selected.classList.add("selected");
+            }
+        };
+
+        // body에 scroll event listener 추가
+        // SSR 에러 방지(typeof document !== "undefined")
+        if (typeof document !== "undefined") {
+            document.body.addEventListener("scroll", scrollHandler);
+        }
+
+        return () => {
+            if (typeof document !== "undefined") {
+                document.body.removeEventListener("scroll", scrollHandler);
+            }
+        };
+    }, []);
+
     return (
         <div
-            className={styles["toc"]}
+            className={styles.toc}
             onClick={handleClick}
             onKeyDown={handleKeyDown}
             role="button"
@@ -30,6 +74,6 @@ const TableOfContents = ({ content }) => {
             dangerouslySetInnerHTML={{ __html: content }}
         />
     );
-}
+};
 
-export default TableOfContents
+export default TableOfContents;
